a1aadafd51109701bfbff0fd868fe61e88d0a39c
drlong <drl1210@163.com>
2018-04-17 01:36:40 +0800

    增加antd-mobileUI

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
-// 新建app
+// // 新建app
 const app = express()
-
+// 登录入口
 app.get('/',function(req,res){
     res.send('<h1>Hellow  DRL </h1>')
 })
@@ -10,5 +10,5 @@ app.get('/data',function(req,res){
 })
 
 app.listen(9093,function () {
-    console.log('port 9093?')
+    console.log('localhost: 9093')
 })
