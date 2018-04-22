6e99233ef34077343f95e0471e3fdcbbc809a21a
drlong <drl1210@163.com>
2018-04-22 23:52:15 +0800

    删除一些东西吧

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends React.Component{
   render(){
     return (
       <div>
-        <h2>现在有机枪{this.props.store.getState()}把</h2>
+        <h2>开始吧啊</h2>
         <button onClick={()=>{this.props.store.dispatch(addGun())}}>申请武器</button>
       </div>
     ) 
